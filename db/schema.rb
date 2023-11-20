a4d28de53b52fa0ffb26c92ed37da2f4346218d2
luis <luisportelatorres@gmail.com>
2023-11-20 14:59:03 +0000

    setup ready to start version 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_145444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.string "status"
+    t.bigint "users_id", null: false
+    t.bigint "venue_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_bookings_on_users_id"
+    t.index ["venue_id"], name: "index_bookings_on_venue_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "name"
+    t.string "description"
+    t.integer "capacity"
+    t.boolean "status"
+    t.bigint "users_id"
+    t.integer "bustability"
+    t.index ["users_id"], name: "index_venues_on_users_id"
+  end
+
+  add_foreign_key "bookings", "users", column: "users_id"
+  add_foreign_key "bookings", "venues"
+  add_foreign_key "venues", "users", column: "users_id"
 end
