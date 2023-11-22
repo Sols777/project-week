b73b4d46100e6628428768b04b3682a5b0236a4d
luis <luisportelatorres@gmail.com>
2023-11-22 11:32:45 +0000

    navigation  buttons added to navbar

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,6 +3,8 @@ class Venue < ApplicationRecord
   has_many :bookings, dependent: :destroy
   # has_many :users, through: :bookings, dependent: :destroy
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :address, presence: true
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_130016) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_110213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_130016) do
     t.boolean "status"
     t.bigint "user_id"
     t.integer "bustability"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
