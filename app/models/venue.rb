4777a0188789937a900f45dea9f07c902efa15fb
luis <luisportelatorres@gmail.com>
2023-11-22 16:35:28 +0000

    changes

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,8 +1,7 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  # has_many :users, through: :bookings, dependent: :destroy
-  has_one_attached :photo
+  has_many :users, through: :bookings, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
