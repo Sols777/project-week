70cede4501115d6f06554ff9c26e690000d876ea
luis <luisportelatorres@gmail.com>
2023-11-22 16:46:08 +0000

    v1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def cloudinary_pic(venue)
     if venue.photo.attached?
-      cl_image_tag venue.photo.key, height: 300, width: 400, crop: :fill
+      cl_image_path venue.photo.key, height: 300, width: 400, crop: :fill
     else
       image_path 'https://thinkfirstcommunication.com/placeholder-1-png/', height: 300, width: 400, crop: :fill
     end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,6 +4,7 @@ class Venue < ApplicationRecord
   has_many :users, through: :bookings, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_one_attached :photo
 
   validates :address, presence: true
   validates :name, presence: true
